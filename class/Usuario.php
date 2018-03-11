f33f0de223f43b031768a6d2a68b8853c1c3bb2c
keji100 <willianjapa100@gmail.com>
2018-03-10 23:22:28 -0300

    Métodos de Lista

--- class/Usuario.php
+++ class/Usuario.php
@@ -43,7 +43,7 @@ class Usuario{
 
 		$sql = new Sql();
 
-		$results = $sql->select("SELECT * FROM tb_usuarios WHERE idusuario = :ID", array(
+		$results = $sql->select("SELECT * FROM tb_usuarios WHERE idusuario = :ID;", array(
 			':ID'=>$id));
 
 		if(count($results) > 0){
@@ -54,6 +54,46 @@ class Usuario{
 			$this->setDeslogin($row['deslogin']);
 			$this->setDessenha($row['dessenha']);
 			$this->setDtcadastro(new dateTime($row['dtcadastro']));
+		}
+
+	}
+
+	public static function getList(){
+
+		$sql = new Sql();
+
+		return $sql->select("SELECT * FROM tb_usuarios ORDER BY deslogin;");
+
+	}
+
+	public static function search($login){
+
+		$sql = new Sql();
+
+		return $sql->select("SELECT * FROM tb_usuarios WHERE deslogin like :LOGIN ORDER BY deslogin;", array(':LOGIN'=>"%".$login."%"));
+	
+	}
+
+	public function login($login, $password){
+
+		$sql = new Sql();
+
+		$results = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin = :LOGIN AND dessenha = :SENHA;", array(
+			':LOGIN'=>$login,':SENHA'=>$password));
+
+		if(count($results) > 0){
+
+			$row = $results[0];
+
+			$this->setIdusuario($row['idusuario']);
+			$this->setDeslogin($row['deslogin']);
+			$this->setDessenha($row['dessenha']);
+			$this->setDtcadastro(new dateTime($row['dtcadastro']));
+		} else {
+
+			throw new Exception("Login ou senha inválido");
+			
+
 		}
 
 	}
--- index.php
+++ index.php
@@ -2,10 +2,24 @@
 
 require_once("config.php");
 
+/*
+//Carrega o usuário:
 $root = new Usuario();
-
 $root->loadbyId(1);
+echo $sql->getList();
+*/
 
-echo $root;
+//Carrega Lista
+//$lista = Usuario::getList();
+//echo json_encode($lista);
 
+//Procurar usuário
+//$busca = Usuario::search("willian");
+//echo json_encode($busca);
+
+//Carrega um usuário usando o login e senha
+$usuario = new Usuario();
+$usuario->login("willian", "lol");
+
+echo $usuario;
 ?>
